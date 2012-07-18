2ece4a2b28effd59e9adbaf033e4d2a22a5a1413
Andy Hedges <andy@hedges.net>
2012-07-18 22:07:44 +0100

    Work on the generic codec and a CLI for it

--- codec-generic/src/main/java/net/hedges/fandangled/codec/generic/GenericCodec.java
+++ codec-generic/src/main/java/net/hedges/fandangled/codec/generic/GenericCodec.java
@@ -6,7 +6,6 @@ import freemarker.template.Template;
 import freemarker.template.TemplateException;
 
 import net.hedges.fandangled.bindings.domain.*;
-import net.hedges.fandangled.bindings.domain.Exception;
 import net.hedges.fandangled.codec.commons.Codec;
 import net.hedges.fandangled.codec.commons.TranscodingException;
 
@@ -24,60 +23,65 @@ import java.util.Map;
 public class GenericCodec implements Codec {
 
     Configuration cfg = new Configuration();
-    
+
     private String templatePath = ".";
     private String extension = ".txt";
     private String prefix = "Fandangled-";
 
     public GenericCodec() {
-        cfg.setObjectWrapper(new DefaultObjectWrapper());
-        cfg.setClassForTemplateLoading(this.getClass(), "/");
+
     }
 
     public void encode(Interface serviceInterface, File outputDirectory) throws TranscodingException {
 
+
+        try {
+            cfg.setObjectWrapper(new DefaultObjectWrapper());
+            cfg.setDirectoryForTemplateLoading(new File(templatePath));
+        } catch (IOException ioe) {
+            throw new TranscodingException("Couldn't find template directory: " + templatePath, ioe);
+        }
+
         File dir = new File(outputDirectory, serviceInterface.getName());
         dir.mkdirs();
 
-        List<Type> types = serviceInterface.getTypes();
-        for (Type type : types) {
-            Map<String, Object> rootMap = new HashMap<String, Object>();
-            rootMap.put("interface", serviceInterface);
-            rootMap.put("type", type);
-            create("Type.ftl", rootMap, new File(dir, prefix + type.getName() + extension));
+        Map<String, Object> rootMap = new HashMap<String, Object>();
+        rootMap.put("interface", serviceInterface);
+        create("interface.ftl", rootMap, new File(dir, prefix + serviceInterface.getName() + extension));
+
+        processEntity(serviceInterface, "type", serviceInterface.getTypes(), dir);
+        processEntity(serviceInterface, "event", serviceInterface.getEvents(), dir);
+        processEntity(serviceInterface, "operation", serviceInterface.getOperations(), dir);
+        processEntity(serviceInterface, "enumeration", serviceInterface.getEnumerations(), dir);
+        processEntity(serviceInterface, "exception", serviceInterface.getExceptions(), dir);
+
+    }
+
+    private <T extends AbstractEntity> void processEntity(Interface serviceInterface, String name, List<T> items, File outputDir) throws TranscodingException {
+        File individualTemplate = new File(templatePath, name + ".ftl");
+        Map<String, Object> rootMap = new HashMap<String, Object>();
+        if (individualTemplate.exists()) {
+            for (T i : items) {
+                rootMap = new HashMap<String, Object>();
+                rootMap.put("interface", serviceInterface);
+                rootMap.put(name, items);
+                create(name + ".ftl", rootMap, new File(outputDir, i.getName() + extension));
+            }
         }
 
-        List<net.hedges.fandangled.bindings.domain.Exception> exceptions = serviceInterface.getExceptions();
-        for (Exception exception : exceptions) {
-            Map<String, Object> rootMap = new HashMap<String, Object>();
+        File groupTemplate = new File(templatePath, name + "s.ftl");
+        if (groupTemplate.exists()) {
+            rootMap = new HashMap<String, Object>();
             rootMap.put("interface", serviceInterface);
-            rootMap.put("exception", exception);
-            create("Exception.ftl", rootMap, new File(dir, prefix + exception.getName() + extension));
+            rootMap.put(name + "s", items);
+            create(name + "s.ftl", rootMap, new File(outputDir, prefix + name + extension));
         }
-
-        List<Enumeration> enumerations = serviceInterface.getEnumerations();
-        for (Enumeration enumeration : enumerations) {
-            Map<String, Object> rootMap = new HashMap<String, Object>();
-            rootMap.put("interface", serviceInterface);
-            rootMap.put("enumeration", enumerations);
-            create("Enumeration.ftl", rootMap, new File(dir, prefix + enumeration.getName() + extension));
-        }
-
-        List<Event> events = serviceInterface.getEvents();
-        for (Event event : events) {
-            Map<String, Object> rootMap = new HashMap<String, Object>();
-            rootMap.put("interface", serviceInterface);
-            rootMap.put("event", enumerations);
-            create("Event.ftl", rootMap, new File(dir, event.getName() + ".cs"));
-        }
-
     }
 
     private void create(String templateName, Map<String, Object> rootMap, File outputFile) throws TranscodingException {
 
         PrintWriter out = null;
         try {
-
             Template template = cfg.getTemplate(templateName);
             out = new PrintWriter(new FileWriter(outputFile));
             template.process(rootMap, out);
@@ -93,4 +97,15 @@ public class GenericCodec implements Codec {
         }
     }
 
+    public void setTemplatePath(String templatePath) {
+        this.templatePath = templatePath;
+    }
+
+    public void setExtension(String extension) {
+        this.extension = extension;
+    }
+
+    public void setPrefix(String prefix) {
+        this.prefix = prefix;
+    }
 }
---
+++ command-line/src/main/java/net/hedges/fandangled/commandline/GenericCli.java
@@ -0,0 +1,148 @@
+/*
+   Copyright 2010 Andy Hedges <andy@hedges.net>
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+package net.hedges.fandangled.commandline;
+
+import net.hedges.fandangled.bindings.builder.FandangledBindingException;
+import net.hedges.fandangled.bindings.builder.InterfaceBuilder;
+import net.hedges.fandangled.bindings.domain.Interface;
+import net.hedges.fandangled.codec.generic.GenericCodec;
+import org.apache.commons.cli.*;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class GenericCli {
+
+    private static Options options;
+    private static boolean verbose = false;
+
+    public static void main(String[] args) {
+        options = buildOptions();
+
+        try {
+            CommandLineParser parser = new GnuParser();
+            CommandLine line = parser.parse(options, args);
+            verbose = line.hasOption('v');
+            if(line.hasOption('h')){
+                usage();
+                System.exit(0);
+            }
+            String template = line.getOptionValue("templates");
+            String input = line.getOptionValue("input");
+            String output = line.getOptionValue("output");
+
+            String prefix = line.getOptionValue("prefix", "");
+            String extension = line.getOptionValue("extension", ".txt");
+
+            File inputFile = new File(input);
+            File outputDir = new File(output);
+            File templateDir = new File(template);
+
+            checkFilesAndDirectories(inputFile, outputDir, templateDir);
+
+            GenericCodec codec = new GenericCodec();
+            codec.setTemplatePath(templateDir.getAbsolutePath());
+            codec.setExtension(extension);
+            codec.setPrefix(prefix);
+
+            Interface _interface = InterfaceBuilder.parse(inputFile);
+
+            codec.encode(_interface, outputDir);
+
+        } catch (ParseException e) {
+            System.err.print(e.getMessage());
+            usage();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void checkFilesAndDirectories(File inputFile, File outputDir, File templateDir){
+            if (!inputFile.isFile() || !inputFile.exists()) {
+                usage();
+                throw new RuntimeException("idl file " + inputFile + " doesn't exist or isn't a file");
+            }
+
+            if (!templateDir.isDirectory() || !templateDir.exists()) {
+                usage();
+                throw new RuntimeException("template directory " + templateDir + " doesn't exist or isn't a directory");
+            }
+
+            outputDir.mkdirs();
+            if (outputDir.isFile() || !outputDir.exists()) {
+                usage();
+                throw new RuntimeException("output directory " + outputDir + " is a file or doesn't exist");
+            }
+
+            if (verbose) {
+                System.out.println("template: " + templateDir.getAbsolutePath());
+                System.out.println("input: " + inputFile.getAbsolutePath());
+                System.out.println("output: " + outputDir.getAbsolutePath());
+            }
+    }
+
+    public static Options buildOptions() {
+        Options options = new Options();
+        options.addOption(
+                OptionBuilder.withLongOpt("templates")
+                        .withDescription("the directory containing freemarker templates")
+                        .hasArg()
+                        .isRequired()
+                        .withArgName("directory")
+                        .create("t"));
+        options.addOption(
+                OptionBuilder.withLongOpt("output")
+                        .withDescription("the directory to which generated files will be outputted")
+                        .hasArg()
+                        .isRequired()
+                        .withArgName("directory")
+                        .create("o"));
+        options.addOption(
+                OptionBuilder.withLongOpt("input")
+                        .withDescription("the Fandangled IDL to use")
+                        .hasArg()
+                        .isRequired()
+                        .withArgName("idl")
+                        .create("i"));
+        options.addOption(
+                OptionBuilder.withLongOpt("prefix")
+                        .withDescription("the prefix to give each file name")
+                        .hasArg()
+                        .withArgName("prefix")
+                        .create("p"));
+        options.addOption(
+                OptionBuilder.withLongOpt("extension")
+                        .withDescription("the extension to give each file name")
+                        .hasArg()
+                        .withArgName("extension")
+                        .create("e"));
+        options.addOption(
+                OptionBuilder.withLongOpt("verbose")
+                        .withDescription("verbose output")
+                        .create("v"));
+        options.addOption(
+                OptionBuilder.withLongOpt("help")
+                        .withDescription("display usage")
+                        .create("h"));
+        return options;
+    }
+
+    public static void usage() {
+        HelpFormatter formatter = new HelpFormatter();
+        formatter.printHelp("generic", "Fandangled generic cli\n", options, "", true);
+    }
+
+}
